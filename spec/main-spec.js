51ecb2c2305ce6f3010f85ab3258295461940b27
e <wlx100715@163.com>
2019-01-02 23:02:01 +0800

    Modify some code and add readme_test.

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -2,7 +2,7 @@ let sinon = require("sinon");
 let main = require("../lib/main");
 let getStudent = require("../lib/getStudent");
 let Student = require("../lib/student");
-let printScoreSheet = require("../lib/printScoreSheet"); 
+let printScoreSheet = require("../lib/printScoreSheet");
 
 describe('main()', () => {
 
@@ -16,29 +16,15 @@ describe('main()', () => {
     });
 });
 
-describe("Student", () => {
-    it("should have field name and id", () => {
-        const student = new Student("张三", 111, [{ course: "语文", score: 100 }, { course: "数学", score: 100 }]);
-        expect(student.name).toEqual("张三");
-        expect(student.id).toEqual(111);
-        expect(student.courses).toEqual([{ course: "语文", score: 100 }, { course: "数学", score: 100 }]);
-    });
-
-    it("should have a method computeSum and a method computeAverage", () => {
-        const student = new Student("张三", 111, [{ course: "语文", score: 100 }, { course: "数学", score: 100 }]);
-        const sum = student.computeSum();
-        const average = student.computeAverage();
-        expect(sum).toEqual(200);
-        expect(average).toEqual(100);
-    });
-});
-
 describe('getStudent()', () => {
 
     it('should display student message', () => {
-        const str = "张三，111，语文：100，数学：100";
+        const str = "张三，111，语文：100，数学：100，英语：100，编程：100";
         const studentMessage = getStudent(str);
-        let message = { name: "张三", id: 111, courses: [{ course: "语文", score: 100 }, { course: "数学", score: 100 }] };
+        let message = {
+            name: "张三", id: 111, courses: [{ course: "语文", score: 100 }, { course: "数学", score: 100 },
+            { course: "英语", score: 100 }, { course: "编程", score: 100 }]
+        };
         expect(studentMessage).toEqual(message);
     });
 
@@ -49,23 +35,38 @@ describe('getStudent()', () => {
     });
 });
 
+describe("Student", () => {
+
+    it("should have a method computeSum and a method computeAverage", () => {
+        const student = new Student("张三", 111, [{ course: "语文", score: 100 }, { course: "数学", score: 100 },
+        { course: "英语", score: 100 }, { course: "编程", score: 100 }]);
+        const sum = student.computeSum();
+        const average = student.computeAverage();
+        expect(sum).toEqual(400);
+        expect(average).toEqual(100);
+    });
+});
+
 describe('printScoreSheet()', () => {
 
     it('should display score sheet', () => {
-        const student1 = new Student("张三", 111, [{ course: "语文", score: 100 }, { course: "数学", score: 100 }]);
-        const student2 = new Student("李四", 112, [{ course: "语文", score: 80 }, { course: "数学", score: 85 }]);
-        const student3 = new Student("王五", 113, [{ course: "语文", score: 90 }, { course: "数学", score: 95 }]);
+        const student1 = new Student("张三", 111, [{ course: "语文", score: 100 }, { course: "数学", score: 100 },
+        { course: "英语", score: 100 }, { course: "编程", score: 100 }]);
+        const student2 = new Student("李四", 112, [{ course: "数学", score: 85 }, { course: "语文", score: 80 },
+        { course: "英语", score: 80 }, { course: "编程", score: 85 }]);
+        const student3 = new Student("王五", 113, [{ course: "语文", score: 90 }, { course: "数学", score: 95 },
+        { course: "编程", score: 95 }, { course: "英语", score: 90 }]);
         const students = [student1, student2, student3];
         const ids = "111, 112"
         const sheet = printScoreSheet(students, ids);
         expect(sheet).toEqual(`成绩单
-姓名|语文|数学|平均分|总分 
+姓名|语文|数学|英语|编程|平均分|总分 
 ========================
-张三|100|100|100|200
-李四|80|85|82.5|165
+张三|100|100|100|100|100|400
+李四|80|85|80|85|82.5|330
 ========================
-全班总分平均数：183.33
-全班总分中位数：185`);
+全班总分平均数：366.67
+全班总分中位数：370`);
     });
 
     it('should display -1', () => {
