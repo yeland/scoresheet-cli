0af9fbaaa4137a9268e1867f36fc76d464d7ad1b
e <wlx100715@163.com>
2019-01-03 21:48:49 +0800

    Modify getStudent

--- spec/get-student-spec.js
+++ spec/get-student-spec.js
@@ -1,20 +1,19 @@
+let Student = require("../lib/student");
 let getStudent = require("../lib/getStudent");
 
 describe('getStudent()', () => {
 
     it('should display student message', () => {
         const str = "张三，111，语文：100，数学：100，英语：100，编程：100";
-        const studentMessage = getStudent(str);
-        let message = {
-            name: "张三", id: 111, courses: [{ course: "语文", score: 100 }, { course: "数学", score: 100 },
-            { course: "英语", score: 100 }, { course: "编程", score: 100 }]
-        };
-        expect(studentMessage).toEqual(message);
+        const student = getStudent(str);
+        let message = new Student("张三", 111, [{ course: "语文", score: 100 }, { course: "数学", score: 100 },
+        { course: "英语", score: 100 }, { course: "编程", score: 100 }]);
+        expect(student).toEqual(message);
     });
 
     it('should display -1', () => {
         const str = "张三，111，语文, 100，数学：100";
-        const studentMessage = getStudent(str);
-        expect(studentMessage).toBe(-1);
+        const student = getStudent(str);
+        expect(student).toBe(-1);
     });
 });
